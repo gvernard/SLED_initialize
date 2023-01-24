d37089aa6205c174d7eda14116084a3ec83d617b
gvernard <gvernard@astro.rug.nl>
2023-01-24 15:07:09 +0100

    Fixed some bugs in the populate script.

--- populate.sh
+++ populate.sh
@@ -27,23 +27,20 @@ python ${spd}/SLED_api/manage.py shell < ${dir}/add_users/populate_db.py
 echo "Adding users...OK"
 
 # Requires the server to be running for the API call
-cd ${dir}/add_lenses
 echo "Adding lenses..."
-touch ../report_add_lenses.txt
+cd ${dir}/add_lenses
 python upload_through_api.py > ../report_add_lenses.txt
 echo "Adding lenses...OK"
 
 # Requires the server to be running for the API call
-cd ${dir}/add_papers
 echo "Adding papers..."
-touch ../report_add_papers.txt
+cd ${dir}/add_papers
 python upload_papers_API.py > ../report_add_papers.txt
 echo "Adding papers...OK"
 
 # Requires the server to be running for the API call
-cd ${dir}/add_collections
 echo "Adding collections..."
-touch ../report_add_collections.txt
+cd ${dir}/add_collections
 python upload_collection.py > ../report_add_collections.txt
 echo "Adding collections...OK"
 
@@ -54,19 +51,19 @@ echo "Adding instruments and bands...OK"
 
 # Requires the server to be running for the API call (through the database_utils.py)
 echo "Adding imaging data..."
-touch ../report_add_imaging.txt
+cd ${dir}/add_data
 python upload_initial_imaging.py ${spd}/SLED_api > ../report_add_imaging.txt
 echo "Adding imaging data...OK"
 
 # Requires the server to be running for the API call (through the database_utils.py)
 echo "Adding spectra..."
-touch ../report_add_spectra.txt
+cd ${dir}/add_data
 python upload_initial_spectra.py ${spd}/SLED_api > ../report_add_spectra.txt
 echo "Adding spectra...OK"
 
 # Requires the server to be running for the API call (through the database_utils.py)
 echo "Adding catalogue data..."
-touch ../report_add_catelogues.txt
+cd ${dir}/add_data
 python upload_initial_catalogues.py ${spd}/SLED_api > ../report_add_catalogues.txt
 echo "Adding catalogue data...OK"
 
