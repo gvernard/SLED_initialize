6ea4e72aafec4eecdfdcfd343a4e8f3c652f6678
gvernard <gvernard@astro.rug.nl>
2023-01-25 09:42:04 +0100

    Correct import of strptime.

--- add_data/database_utils.py
+++ add_data/database_utils.py
@@ -101,7 +101,7 @@ def upload_imaging_to_db_direct(datalist, username):
         if data['exists']:
             imaging.image.name = '/temporary/admin/' + savename
         if 'date_taken' in finaldata.keys():
-            imaging.date_taken = make_aware( datetime.datetime.fromisoformat(datetime.strptime(finaldata['date_taken'])) )
+            imaging.date_taken = make_aware( datetime.datetime.fromisoformat(datetime.datetime.strptime(finaldata['date_taken'])) )
         imaging.save()
 
     return None
@@ -139,7 +139,7 @@ def upload_spectrum_to_db_direct(datalist, username):
         if data['exists']:
             spectrum.image.name = '/temporary/admin/' + savename
         if 'date_taken' in finaldata.keys():
-            spectrum.date_taken = make_aware( datetime.datetime.fromisoformat(datetime.strptime(finaldata['date_taken'])) )
+            spectrum.date_taken = make_aware( datetime.datetime.fromisoformat(datetime.datetime.strptime(finaldata['date_taken'])) )
         else:
             #we apparently made the date taken a NOT_NULL field
             spectrum.date_taken = make_aware(datetime.datetime.fromisoformat('1858-11-17 00:00:00.000'))
@@ -162,7 +162,7 @@ def upload_catalogue_to_db_direct(datalist, username):
         catalogue = Catalogue(**finaldata)
         catalogue.owner_id = Users.objects.get(username=username).id
         if 'date_taken' in finaldata.keys():
-            catalogue.date_taken = make_aware( datetime.datetime.fromisoformat(datetime.strptime(finaldata['date_taken'])) )
+            catalogue.date_taken = make_aware( datetime.datetime.fromisoformat(datetime.datetime.strptime(finaldata['date_taken'])) )
         else:
             #we apparently made the date taken a NOT_NULL field
             catalogue.date_taken = make_aware(datetime.datetime.fromisoformat('1858-11-17 00:00:00.000'))
