dac3194c1b900ccc6ee163d0907061947e176d52
gvernard <gvernard@astro.rug.nl>
2023-02-21 09:20:41 +0100

    Restructuring of the commands while resetting the database.

--- reset/drop_all_tables.sh
+++ reset/drop_all_tables.sh
@@ -20,17 +20,17 @@ MPASS=$($AWK '/^password/{print $3}' $cnf_file)
 TABLES=$($MYSQL -h $MHOST -P $MPORT -u $MUSER -p$MPASS $MDB -e 'show tables' | $AWK '{ print $1}' | $GREP -v '^Tables' )
 
 #$MYSQL -h 127.0.0.1 -P 8888 -u $MUSER -p$MPASS $MDB -e ""
+$MYSQL -h $MHOST -P $MPORT -u $MUSER -p$MPASS $MDB -e "SET FOREIGN_KEY_CHECKS = 0;"
 for t in $TABLES
 do
     echo $t
     #echo "Deleting $t table from $MDB database..."
-    $MYSQL -h $MHOST -P $MPORT -u $MUSER -p$MPASS $MDB -e "SET FOREIGN_KEY_CHECKS = 0;drop table $t"
+    $MYSQL -h $MHOST -P $MPORT -u $MUSER -p$MPASS $MDB -e "DROP TABLE $t"
 done
-
 $MYSQL -h $MHOST -P $MPORT -u $MUSER -p$MPASS $MDB -e "DROP FUNCTION distance_on_sky;"
-SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-$MYSQL -h $MHOST -P $MPORT -u $MUSER -p$MPASS $MDB < $SCRIPTPATH/function_distance_on_sky.sql
 
 
 $MYSQL -h $MHOST -P $MPORT -u $MUSER -p$MPASS $MDB -e "SET FOREIGN_KEY_CHECKS = 1;"
 $MYSQL -h $MHOST -P $MPORT -u $MUSER -p$MPASS $MDB -e "ALTER DATABASE $MDB CHARACTER SET utf8 COLLATE utf8_bin;"
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+$MYSQL -h $MHOST -P $MPORT -u $MUSER -p$MPASS $MDB < $SCRIPTPATH/function_distance_on_sky.sql
