302e24039a2a339bd56cfb749304b5f9fe1bb503
gvernard <gvernard@astro.rug.nl>
2023-03-01 18:08:30 +0100

    Emergency fix to beta testing script.

--- beta_testing_initialization.py
+++ beta_testing_initialization.py
@@ -22,16 +22,18 @@ from lenses.models import Users, Lenses, Imaging, Spectrum
 
 
 # Query the database for users
-users = Users.objects.exclude(username__in=['AnonymousUser','admin']).filter(is_active=True)
+#users = Users.objects.exclude(username__in=['AnonymousUser','admin']).filter(is_active=True)
+users = Users.objects.exclude(username__in=['AnonymousUser','admin','Cameron','Courbin','Giorgos','anupreeta','aymgal','eric_paic']).filter(is_active=True)
 print(users)
+
+#Lenses.objects.all().update(owner=admin,access_level='PUB')
+#Imaging.objects.all().update(owner=admin,access_level='PUB')
+#Spectrum.objects.all().update(owner=admin)
+
+
 
 admin = Users.objects.get(username='admin')
 admin_qset = Users.getAdmin()
-
-
-Lenses.objects.all().update(owner=admin,access_level='PUB')
-Imaging.objects.all().update(owner=admin,access_level='PUB')
-Spectrum.objects.all().update(owner=admin)
 
 justification = "Giving ownership of some objects to play with."
 justification_pri = "Some private objects to play with."
@@ -51,10 +53,11 @@ for user in users:
         pri_lenses = lenses[:N_pri_lenses]
         pub_lenses = lenses[N_pri_lenses:]
         
-        qset = list( Lenses.objects.filter(pk__in=[lens.id for lens in pub_lenses]) )
-        admin.cedeOwnership(qset,u,justification)
+        if len(pub_lenses) > 0:
+            qset = list( Lenses.objects.filter(pk__in=[lens.id for lens in pub_lenses]) )
+            admin.cedeOwnership(qset,u,justification)
         
-        if N_pri_lenses > 0:
+        if len(pri_lenses) > 0:
             qset = Lenses.objects.filter(pk__in=[lens.id for lens in pri_lenses])
             admin.makePrivate(qset,justification_pri)
             admin.cedeOwnership(list(qset),u,justification)
@@ -72,10 +75,11 @@ for user in users:
         pri_imagings = imagings[:N_pri_imaging]
         pub_imagings = imagings[N_pri_imaging:]
         
-        qset = list( Imaging.objects.filter(pk__in=[imaging.id for imaging in pub_imagings]) )
-        admin.cedeOwnership(qset,u,justification)
+        if len(pub_imagings) > 0:
+            qset = list( Imaging.objects.filter(pk__in=[imaging.id for imaging in pub_imagings]) )
+            admin.cedeOwnership(qset,u,justification)
 
-        if N_pri_imaging > 0:
+        if len(pri_imagings) > 0:
             qset = Imaging.objects.filter(pk__in=[imaging.id for imaging in pri_imagings])
             admin.makePrivate(qset,justification_pri)
             admin.cedeOwnership(list(qset),u,justification)
