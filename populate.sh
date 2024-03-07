ab5fd334ff90e2aef286909644d3d70e864fc62f
Giorgos <gvernardos@amnh.org>
2024-03-07 11:28:23 -0500

    Modified populate script.

--- populate.sh
+++ populate.sh
@@ -2,61 +2,27 @@
 
 if [ $# != 2 ]
 then
-    echo "Two command line arguments are required: "
-    echo "  1 - the database to reset: 'test' or 'production' or 'sqlite'"
-    echo "  2 - the full path to the SLED project directory, i.e. the directory containing SLED_api"
+    echo "One command line argument is required: "
+    echo "  1 - the full path to the SLED project directory, i.e. the directory containing SLED_api"
     exit 0
 fi
-database=$1
-spd=${2%/} # This has to be the SLED project dir, i.e. the directory containing SLED_api
+spd=${1%/} # This has to be the SLED project dir, i.e. the directory containing SLED_api
 dir=$(pwd)
-launch=${spd}/SLED_operations/launch_server
 
 
 echo $spd
 echo $dir
-
 
 # Check host name - needed for calls to the API by the upload scripts
 host_ip=`python -c 'import socket; print(socket.gethostbyname(socket.gethostname()))'`
 echo "Host IP is: " $host_ip
 
 
-# Activate SLED environment
-eval "$(conda shell.bash hook)"
-if [ `hostname -s` = "django01" ]
-then
-    conda activate /projects/astro/sled/SLED_environment
-else
-    conda activate SLED_environment
-fi
-
-
-export DJANGO_SECRET_KEY=`cat ${launch}/secret_key.txt`
-export DJANGO_EMAIL_PASSWORD=`cat ${launch}/email_password.txt`   
-export DJANGO_STATIC_ROOT=/projects/astro/sled/STATIC
-export DJANGO_DOMAIN_NAME=sled.astro.unige.ch
-if [ $database = "test" ]
-then
-    export DJANGO_MEDIA_ROOT=/projects/astro/sled/FILES_TEST
-    export DJANGO_DB_FILE=${launch}/test_server.cnf
-    cp ${launch}/settings_debug.py ${spd}/SLED_api/mysite/settings.py
-else
-    export DJANGO_MEDIA_ROOT=/projects/astro/sled/FILES
-    export DJANGO_DB_FILE=${launch}/test_localhost.cnf
-    export DJANGO_NO_LAST_LOGIN=false
-    cp ${launch}/settings_server_root.py ${spd}/SLED_api/mysite/settings.py
-fi
-
-
-if [ $database = "sqlite" ]
-then
-    cp ${launch}/settings_localhost_sqlite.py ${spd}/SLED_api/mysite/settings.py
-fi
 
 echo "Adding users..."
-python ${spd}/SLED_api/manage.py shell < ${dir}/add_users/populate_db.py
+sudo python ${spd}/SLED_api/manage.py shell < ${dir}/add_users/populate_db.py
 echo "Adding users...OK"
+exit
 
 echo "Adding lenses..."
 cd ${dir}/add_lenses
