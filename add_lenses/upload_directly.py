da2faf48a2ba7dfa9de184358b3151500f041445
Giorgos <gvernardos@amnh.org>
2024-03-08 15:53:24 -0500

    Attempt to fix bug.

--- add_lenses/upload_directly.py
+++ add_lenses/upload_directly.py
@@ -254,7 +254,7 @@ for zzz, eachcsv in enumerate(csvs):
     for datum in form_data:
         for key, value in datum.items():
             if key not in ['mugshot']:
-                print(value)
+                print(key,value)
     r  = c.post('/api/upload-lenses/', data=form_data, content_type="application/json")
     print('OOOKKK')
     
