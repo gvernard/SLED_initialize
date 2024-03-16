4cbb13abe8177f1aa40f6fe4c959af8e60a01681
Giorgos <gvernardos@amnh.org>
2024-03-15 22:18:45 -0400

    Inject up to papers.

--- populate.sh
+++ populate.sh
@@ -19,35 +19,34 @@ echo "Host IP is: " $host_ip
 
 
 
-echo "Adding users..."
-sudo -E python3 ${root_path}/SLED_api/manage.py shell < ${dir}/add_users/populate_db.py
-echo "Adding users...OK"
+#echo "Adding users..."
+#sudo -E python3 ${root_path}/SLED_api/manage.py shell < ${dir}/add_users/populate_db.py
+#echo "Adding users...OK"
 
-echo "Adding lenses..."
-cd ${dir}/add_lenses
-sudo -E python3 ${root_path}/SLED_api/manage.py shell < upload_directly.py > ../report_add_lenses.txt
-echo "Adding lenses...OK"
-exit
+#echo "Adding lenses..."
+#cd ${dir}/add_lenses
+#sudo -E python3 ${root_path}/SLED_api/manage.py shell < upload_directly.py > ../report_add_lenses.txt
+#echo "Adding lenses...OK"
 
-# echo "Adding instruments and bands..."
-# cd ${dir}/add_data
-# sudo -E python3 ${root_path}/SLED_api/manage.py shell < add_instruments_bands.py
-# echo "Adding instruments and bands...OK"
+echo "Adding instruments and bands..."
+cd ${dir}/add_data
+sudo -E python3 ${root_path}/SLED_api/manage.py shell < add_instruments_bands.py
+echo "Adding instruments and bands...OK"
 
-# echo "Adding queries..."
-# cd ${dir}/add_queries
-# sudo -E python3 ${root_path}/SLED_api/manage.py shell < create_queries.py
-# echo "Adding queries...OK"
+echo "Adding queries..."
+cd ${dir}/add_queries
+sudo -E python3 ${root_path}/SLED_api/manage.py shell < create_queries.py
+echo "Adding queries...OK"
 
-# echo "Adding collections..."
-# cd ${dir}/add_collections
-# sudo -E python3 ${root_path}/SLED_api/manage.py shell < upload_collection.py > ../report_add_collections.txt
-# echo "Adding collections...OK"
+echo "Adding collections..."
+cd ${dir}/add_collections
+sudo -E python3 ${root_path}/SLED_api/manage.py shell < upload_collection.py > ../report_add_collections.txt
+echo "Adding collections...OK"
 
-# echo "Adding papers..."
-# cd ${dir}/add_papers
-# sudo -E python3 ${root_path}/SLED_api/manage.py shell < upload_papers_API.py > ../report_add_papers.txt
-# echo "Adding papers...OK"
+echo "Adding papers..."
+cd ${dir}/add_papers
+sudo -E python3 ${root_path}/SLED_api/manage.py shell < upload_papers_API.py > ../report_add_papers.txt
+echo "Adding papers...OK"
 
 # echo "Adding redshifts..."
 # cd ${dir}/add_data
